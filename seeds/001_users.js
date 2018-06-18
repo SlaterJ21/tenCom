4266605f9f2b7f8ef9ad792177cda16365418a1c
Jeff S <Slater.Jeff7@gmail.com>
2018-06-18 14:47:22 -0600

    routes to specify tenant or pm

--- seeds/001_users.js
+++ seeds/001_users.js
@@ -5,7 +5,7 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('users').insert([
-        {id: 1, first_name: 'Jeff', last_name: 'Slater', phone_number: 5165513722, email: 'slater.jeff7@gmail.com', password: '$2a$10$7g.Z824wl7CS0TIxjbpS.e4HKYKXOltK.pVpIebs/iIwRUOtU/Xja', ispm: false},
+        {id: 1, first_name: 'Jeff', last_name: 'Slater', phone_number: 5165513722, email: 'slater.jeff7@gmail.com', password: '$2a$10$7g.Z824wl7CS0TIxjbpS.e4HKYKXOltK.pVpIebs/iIwRUOtU/Xja', ispm: true},
         {id: 2, first_name: 'Johnny', last_name: 'Castillo', phone_number: 9549989650, email: 'johnnycast@gmail.com', password: 'caca124', ispm: true}
       ]);
     });
