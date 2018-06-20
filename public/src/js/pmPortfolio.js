8786624bd3e5a1e567f590dc6074a119c052dd04
John Stephens-Webb <JohnStephensWebb@Johns-MacBook-Pro-2.local>
2018-06-20 10:07:53 -0600

    Bug Fix

--- public/src/js/pmPortfolio.js
+++ public/src/js/pmPortfolio.js
@@ -35,46 +35,46 @@ document.addEventListener("DOMContentLoaded", function(event) {
 //
 // console.log(calculator.add(5,5))
 
-$.getJSON("items.json", function(content){
-      console.log(content)
-
-
-      for (var i = content.length-3; i < content.length; i++) {
-      let cardHtml =
-        `<div class="col s12 m4">
-          <div class="card">
-            <div class="card-image">
-              <img src="${content[i].img}">
-              <span class="card-title">${content[i].title}</span>
-            </div>
-            <div class="card-content">
-              <p>${content[i].cardContent}</p>
-            </div>
-            <div class="card-action">
-              <a class="waves-effect waves-light btn modal-trigger"     href="#${content[i].id}">Details</a>
-
-              <!-- Modal Structure -->
-              <div id="${content[i].id}" class="modal modal-fixed-footer">
-                <div class="modal-content">
-                  <h4>${content[i].modalHeader}</h4>
-                  <p>${content[i].modalContent}</p>
-                </div>
-                <div class="modal-footer">
-                  <a href="#!" class="modal-action modal-close waves-effect waves-green btn-flat ">Close</a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>`
-
-
-        let homeCards = $("#cards")
-        homeCards.append(cardHtml)
-      }
-      // $('#modal4').modal();
-      // $('#modal5').modal();
-      // $('#modal6').modal();
-    })
+// $.getJSON("items.json", function(content){
+//       console.log(content)
+//
+//
+//       for (var i = content.length-3; i < content.length; i++) {
+//       let cardHtml =
+//         `<div class="col s12 m4">
+//           <div class="card">
+//             <div class="card-image">
+//               <img src="${content[i].img}">
+//               <span class="card-title">${content[i].title}</span>
+//             </div>
+//             <div class="card-content">
+//               <p>${content[i].cardContent}</p>
+//             </div>
+//             <div class="card-action">
+//               <a class="waves-effect waves-light btn modal-trigger"     href="#${content[i].id}">Details</a>
+//
+//               <!-- Modal Structure -->
+//               <div id="${content[i].id}" class="modal modal-fixed-footer">
+//                 <div class="modal-content">
+//                   <h4>${content[i].modalHeader}</h4>
+//                   <p>${content[i].modalContent}</p>
+//                 </div>
+//                 <div class="modal-footer">
+//                   <a href="#!" class="modal-action modal-close waves-effect waves-green btn-flat ">Close</a>
+//                 </div>
+//               </div>
+//             </div>
+//           </div>
+//         </div>`
+//
+//
+//         let homeCards = $("#cards")
+//         homeCards.append(cardHtml)
+//       }
+//       // $('#modal4').modal();
+//       // $('#modal5').modal();
+//       // $('#modal6').modal();
+//     })
 
     $(".button-collapse").sideNav();
 
